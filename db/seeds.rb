091ac235029dca29b90d1a5f364cec3bb77b2364
Buyaki <rittahbuyaki@gmail.com>
2022-04-28 21:50:10 +0300

    Add to the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create(:message => 'Hello Beautiful!')
+Greeting.create(:message => 'Hello Handsome!')
+Greeting.create(:message => 'Hello Pearl!')
+Greeting.create(:message => 'Hello Coder!')
+Greeting.create(:message => 'Hello Brilliant!')
